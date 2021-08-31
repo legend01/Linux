62a54600826331b05238bd66beeeaffd58259584
HappyCoding <lg95312lg@outlook.com>
2021-08-31 22:48:04 +0800

    [NXP官方例程]1.将官方NXP例程修改驱动配置

--- Uboot/include/generated/timestamp_autogenerated.h
+++ Uboot/include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Jul 21 2021"
-#define U_BOOT_TIME "22:09:16"
+#define U_BOOT_DATE "Jul 25 2021"
+#define U_BOOT_TIME "16:54:08"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "07/21/2021"
+#define U_BOOT_DMI_DATE "07/25/2021"
