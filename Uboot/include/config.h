1c53457072143bbf1f70d8fa6303f69cfacf7b0f
HappyCoding <lg95312lg@outlook.com>
2021-09-05 12:05:14 +0800

    [修改代码]1.删除部分重复内容

--- Uboot/include/config.h
+++
@@ -1,9 +0,0 @@
-/* Automatically generated - do not edit */
-#define CONFIG_IMX_CONFIG	board/freescale/mx6ullevk/imximage-ddr512.cfg
-#define CONFIG_MX6ULL_EVK_EMMC_REWORK	1
-#define CONFIG_BOARDDIR board/freescale/mx6ullevk
-#include <config_defaults.h>
-#include <config_uncmd_spl.h>
-#include <configs/mx6ullevk.h>
-#include <asm/config.h>
-#include <config_fallbacks.h>
--- Uboot/include/generated/timestamp_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define U_BOOT_DATE "Jul 25 2021"
-#define U_BOOT_TIME "16:54:08"
-#define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "07/25/2021"
