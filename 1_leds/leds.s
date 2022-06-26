70281f9c72cd74891f8f74ab3fbf6e84bf30182f
HappyCoding <lg95312lg@outlook.com>
2022-06-26 18:10:36 +0800

    [提交代码] 1.创建leds.s文件 2.根据芯片手册,利用汇编向相应寄存器写入值

---
+++ 1_leds/leds.s
@@ -0,0 +1,49 @@
+.global _start @全局标号
+
+_start:
+    /* 使能所有外设时钟 */
+    ldr r0, =0x020c4068 @CCGR0
+    ldr r1, =0xffffffff @要向CCGR0写入的数据
+    str r1, [r0] @将0xffffffff写入到CCGR0
+
+    ldr r0, =0x020c406c @CCGR1
+    str r1, [r0] @将0xffffffff写入到CCGR1
+
+    ldr r0, =0x020c4070 @CCGR2
+    str r1, [r0] @将0xffffffff写入到CCGR1
+
+    ldr r0, =0x020c4074 @CCGR3
+    str r1, [r0] @将0xffffffff写入到CCGR1
+
+    ldr r0, =0x020c4078 @CCGR4
+    str r1, [r0] @将0xffffffff写入到CCGR1
+
+    ldr r0, =0x020c407c @CCGR5
+    str r1, [r0] @将0xffffffff写入到CCGR1
+
+    ldr r0, =0x020c4080 @CCGR6
+    str r1, [r0] @将0xffffffff写入到CCGR1
+
+    /* 配置 GPIO1_IO3 PIN的复用为GPIO,也就是
+    *  设置IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO03 = 0x05
+    *  寄存器地址为0x020E0068
+     */
+    ldr r0, =0x020E0068 @IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO03
+    ldr r1, =0x05       @要写入的数据
+    str r1, [r0]        @将0x05写入到寄存器IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO03中
+
+    /* 配置GPIO1_IO03的电气属性
+    *  IOMUXC_SW_PAD_CTL_PAD_GPIO1_IO03
+    *  IOMUXC_SW_PAD_CTL_PAD_GPIO1_IO03寄存器地址为:0x020E02F4
+    * bit0: SRE 0 低速率
+    * bit5:3 DSE 110 R0/6驱动能力
+    * bit6:7 SPEED 10 100M速率
+    * bit11  DOE   0  关闭开路输出
+    * bit12  PKE   1  使能pull/keeper
+    * bit13  PUE   0  keeper 
+    * bit15:14 PUS  00 100K下拉
+    * bit16    HYS  0  关闭HYS
+     */
+    ldr r0, =0x020E02F4
+    ldr r1, =0x10B0
+    str r1, [r0]
